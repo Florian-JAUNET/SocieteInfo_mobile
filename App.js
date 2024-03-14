5f5a551ae9126b1946fc6e5aa1f1c341c2ba6c6a
flo_flooff <floflo.off@gmail.com>
2024-03-14 14:27:15 +0100

    Add different path to screens

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import HomeScreen from "./src/screen/HomeScreen";
+import InfoScreen from "./src/screen/InfoScreen";
+import LoginScreen from "./src/screen/LoginScreen";
+import ResearchAdresseScreen from "./src/screen/ResearchAdresseScreen";
+import ResearchNameScreen from "./src/screen/ResearchNameScreen";
+import ResearchSiretScreen from "./src/screen/ResearchSiretScreen";
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+};
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Accueil", headerLeft: false }}
+        />
+        <Stack.Screen
+          name="ResearchName"
+          component={ResearchNameScreen}
+          options={{
+            title: "Recherche par nom",
+            headerTitleStyle: { fontSize: 18 },
+          }}
+        />
+        <Stack.Screen
+          name="ResearchSiret"
+          component={ResearchSiretScreen}
+          options={{
+            title: "Recherche par SIREN/SIRET/TVA",
+            headerTitleStyle: { fontSize: 18 },
+          }}
+        />
+        <Stack.Screen
+          name="ResearchAdresse"
+          component={ResearchAdresseScreen}
+          options={{
+            title: "Recherche par adresse",
+            headerTitleStyle: { fontSize: 18 },
+          }}
+        />
+        <Stack.Screen
+          name="Info"
+          component={InfoScreen}
+          options={{
+            title: "Informations",
+            headerTitleStyle: { fontSize: 18 },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
